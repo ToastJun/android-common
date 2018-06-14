981856033b76d43dfb87bcba2da83d070fe1a4d8
toastjun <toastjun@163.com>
2018-06-14 23:43:19 +0800

    使用jdk1.8 Rxhelper改用lambda

--- core/src/main/java/com/toast/core/net/RxHelper.java
+++ core/src/main/java/com/toast/core/net/RxHelper.java
@@ -5,9 +5,7 @@ import android.util.Log;
 import com.toast.core.net.bean.BaseNetBean;
 
 import rx.Observable;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -15,40 +13,37 @@ import rx.schedulers.Schedulers;
  */
 public class RxHelper {
 
-    public static <T>Observable.Transformer<BaseNetBean<T>, T> handleResult(){
-        return new Observable.Transformer<BaseNetBean<T>, T>(){
-
-            @Override
-            public Observable<T> call(final Observable<BaseNetBean<T>> baseNetBeanObservable) {
-                return baseNetBeanObservable.flatMap(new Func1<BaseNetBean<T>, Observable<T>>() {
-                    @Override
-                    public Observable<T> call(BaseNetBean<T> tBaseNetBean) {
-                        Log.i("RxHelper", tBaseNetBean.code + "");
-                        if (tBaseNetBean.code == 0) {
-                            return createData(tBaseNetBean.data);
-                        } else {
-                            return Observable.error(new Exception(tBaseNetBean.error));
-                        }
+    /**
+     * 将 通用的网络返回类 转换成 其中的data
+     * @param <T>
+     * @return
+     */
+    public static <T> Observable.Transformer<BaseNetBean<T>, T> handleResult(){
+        return baseNetBeanObservable -> baseNetBeanObservable.flatMap(
+                tBaseNetBean -> {
+                    Log.i("RxHelper", tBaseNetBean.code + "");
+                    if (tBaseNetBean.code == 0) {
+                        return createData(tBaseNetBean.data);
+                    } else {
+                        return Observable.error(new Exception(tBaseNetBean.error));
                     }
-                }).subscribeOn(Schedulers.io())
-                        .unsubscribeOn(Schedulers.io())
-                        .subscribeOn(AndroidSchedulers.mainThread())
-                        .observeOn(AndroidSchedulers.mainThread());
-            }
-        };
+                }
+        ).compose(switchSchedulers());
     }
 
     private static <T> Observable<T> createData(final T data){
-        return Observable.create(new Observable.OnSubscribe<T>() {
-            @Override
-            public void call(Subscriber<? super T> subscriber) {
+        return Observable.create(subscriber ->  {
                 try {
                     subscriber.onNext(data);
                     subscriber.onCompleted();
                 } catch (Exception e) {
                     subscriber.onError(e);
                 }
-            }
         });
+    }
+
+    public static <T> Observable.Transformer<T, T> switchSchedulers() {
+        return tObservable -> tObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
