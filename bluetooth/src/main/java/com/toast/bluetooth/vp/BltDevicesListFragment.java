de84388ddc72fc5f962eb9e70697570645012797
wwj <wwj00d@163.com>
2018-07-13 10:29:49 +0800

    蓝牙相关开发

---
+++ bluetooth/src/main/java/com/toast/bluetooth/vp/BltDevicesListFragment.java
@@ -0,0 +1,7 @@
+package com.toast.bluetooth.vp;
+
+import android.support.v4.app.DialogFragment;
+
+public class BltDevicesListFragment extends DialogFragment {
+
+}
---
+++ common/src/main/java/com/toast/common/dialog/BaseBottomDialogFragment.java
@@ -0,0 +1,7 @@
+package com.toast.common.dialog;
+
+import android.support.v4.app.DialogFragment;
+
+public class BaseBottomDialogFragment extends DialogFragment {
+
+}
